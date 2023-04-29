a653d76bcae40eb1f42379c766173ecebef363fc
sime-81 <yapock123@gmail.com>
2023-04-29 18:58:19 +0200

    cours sur les mouses event et keypress event

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const btn2 = document.getElementById("btn-2");
 const response = document.querySelector("p");
 
 questionContainer.addEventListener("click", () => {
-  questionContainer.classList.add("question-click");
+  questionContainer.classList.toggle("question-click");
 });
 
 btn1.addEventListener("click", () => {
@@ -23,4 +23,51 @@ btn2.addEventListener("click", () => {
   response.style.background = "red";
 });
 
+/*
+Ordre de priorité d'application css 
+
+<div> > #id > .class > baliseHTML  
+
+*/
+
 // --------------------------------------------------------
+// Mouse event
+
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  mousemove.style.transform = "scale(1.3) translate(-39%, -39%)";
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
+
+// Je préfère mouseover pour éviter les bugs de survole d'autre élément
+questionContainer.addEventListener("mouseenter", () => {
+  questionContainer.style.background = "rgba(0, 0, 0, 0.6)";
+});
+
+questionContainer.addEventListener("mouseout", () => {
+  questionContainer.style.background = "rgba(0, 0, 0, 1)";
+});
+
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(2deg)";
+});
+
+// --------------------------------------------------------
+// KeyPress event
+
+const keypreesContainer = document.querySelector(".keypress");
+const key = document.getElementById("key");
+
+document.addEventListener('keypress', (e) => {
+  key.textContent = e.key
+})
